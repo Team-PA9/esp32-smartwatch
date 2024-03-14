8948f3ec3dd21dc3b3441978310643385b33fac7
LilyNash <valentine.vessella@hotmail.fr>
2024-03-14 21:08:04 +0100

    Sensors V0
    Code compiling without warning or mistakes.
    Still to do : add timer acquisition and intern RTC.

--- main/main.c
+++ main/main.c
@@ -1,20 +1,61 @@
-#include "esp_log.h"
+/** ----------------------------------------------------------------------------
+ * Project:     ESP32 - SmartWatch
+ * File:        main.c
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Author:      PA9 TEAM
+ * Modified:    11/03/2024
+ * Framework:   ESP32 (Wrover Kit) v5.1.2
+ * -------------------------------------------------------------------------- */
+
+
+/* -----------------------------------------------------------------------------
+ * PART 0 : Includes
+ * -------------------------------------------------------------------------- */
+#include <stdio.h>
+// --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
 #include "bsp/esp_wrover_kit.h"
-#include "lvgl.h"
-#include "ui/ui.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "lvgl.h"
+#include "sdkconfig.h"
+// --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+#include "sensors/sensors.h"
+#include "sensors/lis2mdl_reg.h"
+#include "sensors/lsm6dso_reg.h"
+#include "ui/ui.h"
 
-#define TAG "SMARTWATCH"
+
+/* -----------------------------------------------------------------------------
+ * PART 1 : Global Defines, Variables & Structures
+ * -------------------------------------------------------------------------- */
+static const char *TAG = "SMARTWATCH";
+
+// Display
 #define APP_DISP_DEFAULT_BRIGHTNESS 50
-
-
 static lv_disp_t *display;
 
-/*******************************************************************************
-* Private functions
-*******************************************************************************/
+// Sensors
+extern stmdev_ctx_t lsm6dso_dev_ctx, lis2mdl_dev_ctx;
+extern uint8_t LSM6DSO_whoamI, LIS2MDL_whoamI;
+extern bool LSM6DSO_OK, LIS2MDL_OK;
 
+
+/* -----------------------------------------------------------------------------
+ * PART 2 : Global Handlers
+ * -------------------------------------------------------------------------- */
+TaskHandle_t xHandle_lvgl = NULL;
+TaskHandle_t xHandle_whoami = NULL;
+TaskHandle_t xHandle_getdata = NULL;
+
+
+/* -----------------------------------------------------------------------------
+ * PART 3 : Private Functions
+ * -------------------------------------------------------------------------- */
+
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Task : LVGL initialize and handle
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
 void lvgl_task(void *pvParameter) {
     // Initialize UI
     bsp_display_lock(0);
@@ -29,19 +70,82 @@ void lvgl_task(void *pvParameter) {
     }
 }
 
-void app_main(void) {
-    // Initialize display and LVGL
-    display = bsp_display_start();
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Task : Get data from sensors
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+void get_data_task(void *args) {
+    while (1) {
+        if (LSM6DSO_OK == 1) { get_LSM6DSO(); }
+        if (LIS2MDL_OK == 1) { get_LIS2MDL(); }
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
 
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Task : WhoAmI, check if sensor is detected on I2C
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+void whoami_task(void *args) {
+    static const char *TAG = "WhoAmI";
+
+    while (1) {
+        ESP_LOGI(TAG, "Master check sensors ---");
+
+        // LSM6DSO ID
+        lsm6dso_device_id_get(&lsm6dso_dev_ctx, &LSM6DSO_whoamI);
+        if (LSM6DSO_whoamI != LSM6DSO_ID) { ESP_LOGE(TAG, "LSM6DSO not find"); }
+        else {
+            printf("Product ID= %d \n", LSM6DSO_whoamI);
+            LSM6DSO_OK = 1;
+            }
+
+        // LIS2MDL ID
+        lis2mdl_device_id_get(&lis2mdl_dev_ctx, &LIS2MDL_whoamI);
+        if (LIS2MDL_whoamI != LIS2MDL_ID) { ESP_LOGE(TAG, "LIS2MDL not find"); }
+        else {
+            printf("Product ID= %d \n", LIS2MDL_whoamI);
+            LIS2MDL_OK = 1;
+            }
+
+        xTaskCreate(get_data_task, "get_data_task", 2048, NULL, 1, &xHandle_getdata);
+
+        ESP_LOGI(TAG, "End of check ---");
+        vTaskDelete(xHandle_whoami);
+        fflush(stdout);
+  }
+}
+
+
+/* -----------------------------------------------------------------------------
+ * PART 4 : app_main()
+ * -------------------------------------------------------------------------- */
+void app_main(void) {
+    ESP_LOGI(TAG, "Started app_main() ---");
+
+    // --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+    // Step 0: Initialize Display & LVGL
+    // --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+    printf("Initialize display and LVGL ...\n");
+    display = bsp_display_start();
     // Rotate display 90 degrees (landscape mode)
     bsp_display_rotate(display, 90);
     lv_disp_set_rotation(display, LV_DISP_ROT_90);
-
     // Turn on display backlight
     bsp_display_brightness_set(APP_DISP_DEFAULT_BRIGHTNESS);
-
     // Create FreeRTOS task for LVGL
-    xTaskCreate(&lvgl_task, "lvgl_task", 4096, NULL, 1, NULL);
+    xTaskCreate(&lvgl_task, "lvgl_task", 4096, NULL, 1, &xHandle_lvgl);
 
-    ESP_LOGI(TAG, "Example initialization done.");
+    // --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+    // Step 1: Initialize I2C & Sensors
+    // --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+    printf("Initialize I2C master... \n");
+    ESP_ERROR_CHECK(i2c_master_init());
+    printf("Initialize LSM6DSO sensor... \n");
+    ESP_ERROR_CHECK(lsm6dso_init());
+    printf("Initialize LIS2MDL sensor... \n");
+	ESP_ERROR_CHECK(lis2mdl_init());
+    // Create whoamI task for sensors on I2C
+    xTaskCreate(whoami_task, "whoami_task", 2048, NULL, 1, &xHandle_whoami);
+
+    ESP_LOGI(TAG, "Ended app_main() ---");
+    fflush(stdout);
 }
---
+++ main/sensors/sensors.c
@@ -0,0 +1,318 @@
+/** ----------------------------------------------------------------------------
+ * Project:     ESP32 - SmartWatch
+ * File:        sensors.c
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Author:      PA9 TEAM
+ * Modified:    11/03/2024
+ * Framework:   ESP32 (Wrover Kit) v5.1.2
+ * -------------------------------------------------------------------------- */
+
+
+/* -----------------------------------------------------------------------------
+ * PART 0 : Includes
+ * -------------------------------------------------------------------------- */
+#include <string.h>
+// --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+#include "driver/i2c.h"
+#include "esp_log.h"
+// --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+#include "sensors.h"
+#include "sensors/lis2mdl_reg.h"
+#include "sensors/lsm6dso_reg.h"
+
+
+/* -----------------------------------------------------------------------------
+ * PART 1 : Global Defines, Variables & Structures
+ * -------------------------------------------------------------------------- */
+// I2C
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+
+#define I2C_MASTER_SCL_IO CONFIG_I2C_MASTER_SCL                                 /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO CONFIG_I2C_MASTER_SDA                                 /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUMBER(CONFIG_I2C_MASTER_PORT_NUM)                   /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ CONFIG_I2C_MASTER_FREQUENCY                          /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0                                             /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0                                             /*!< I2C master doesn't need buffer */
+
+#define LIS2MDL_ADDR_7BITS 0x1e													/*!< LSM6DSO I2C Address : 8bits = 3C (0011 1100) -> 7bits = 1E (001 1110) */
+#define LSM6DSO_ADDR_7BITS 0x6b                                                 /*!< LSM6DSO I2C Address : 8bits = D4 (1110 0110) -> 7bits = 73 (111 0011) */
+#define SENSOR_BUS I2C_MASTER_NUM                                               /*!< I2C port number for master dev */
+#define WRITE_BIT I2C_MASTER_WRITE                                              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                                                /*!< I2C master read */
+#define ACK_CHECK_EN 0x1                                                        /*!< I2C master will check ack from slave */
+#define ACK_CHECK_DIS 0x0                                                       /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                                                             /*!< I2C ack value */
+#define NACK_VAL 0x1                                                            /*!< I2C nack value */
+
+// LSM6MDL
+stmdev_ctx_t lsm6dso_dev_ctx;
+bool LSM6DSO_OK;
+uint8_t LSM6DSO_whoamI, LSM6DSO_rst;
+int16_t LSM6DSO_raw_acceleration[3];
+int16_t LSM6DSO_raw_angular_rate[3];
+int16_t LSM6DSO_raw_temperature;
+float LSM6DSO_acceleration_mg[3];
+float LSM6DSO_angular_rate_mdps[3];
+float LSM6DSO_temperature_degC;
+
+// LIS2MDL
+stmdev_ctx_t lis2mdl_dev_ctx;
+bool LIS2MDL_OK;
+uint8_t LIS2MDL_whoamI;
+int16_t LIS2MDL_raw_magnetic[3];
+float LIS2MDL_magnetic_mG[3];
+
+// Log Arrays & Indexs
+float log_temperature[100];
+float log_magnetic_X[100];
+float log_magnetic_Y[100];
+float log_magnetic_Z[100];
+float log_acceleration_X[100];
+float log_acceleration_Y[100];
+float log_acceleration_Z[100];
+float log_angular_rate_X[100];
+float log_angular_rate_Y[100];
+float log_angular_rate_Z[100];
+
+uint8_t mag_index = 0;
+uint8_t acc_index = 0;
+uint8_t gyro_index = 0;
+uint8_t temp_index = 0;
+
+// Others
+uint8_t tx_buffer[1000];
+
+
+
+/* -----------------------------------------------------------------------------
+ * PART 2 : Private Functions
+ * -------------------------------------------------------------------------- */
+
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Function : Initialize I2C master
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+esp_err_t i2c_master_init(void) {
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+
+    esp_err_t err = i2c_param_config(i2c_master_port, &conf);
+    if (err != ESP_OK) { return err; }
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Function : Read data from the sensor with I2C communication.
+ * _____________________________________________________________________________
+ * | start | slave_addr + wr_bit + ack | write 1 byte (register address) + ack |
+ * --------|---------------------------|---------------------------------------|
+ *      _________________________________________________________________
+ * ...   start | slave_addr + rd_bit + ack | read n bytes + nack | stop |
+ *      -------|---------------------------|---------------------|------|
+ *
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+int32_t i2c_master_read_LSM6DSO(uint8_t i2c_num, uint8_t regaddr, uint8_t *data_rd, uint16_t size) {
+    if (size == 0) { return ESP_OK; }
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (LSM6DSO_ADDR_7BITS << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, regaddr, ACK_CHECK_EN);
+
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (LSM6DSO_ADDR_7BITS << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
+    if (size > 1) { i2c_master_read(cmd, data_rd, size - 1, ACK_VAL); }
+    i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+int32_t i2c_master_read_LIS2MDL(uint8_t i2c_num, uint8_t regaddr, uint8_t *data_rd, uint16_t size) {
+    if (size == 0) { return ESP_OK; }
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (LIS2MDL_ADDR_7BITS << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, regaddr, ACK_CHECK_EN);
+
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (LIS2MDL_ADDR_7BITS << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
+    if (size > 1) { i2c_master_read(cmd, data_rd, size - 1, ACK_VAL); }
+    i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Function : Write data to the sensor with I2C communication.
+ * _____________________________________________________________________________
+ * | start | slave_addr + wr_bit + ack | write 1 byte (register address) + ack |
+ * --------|---------------------------|---------------------------------------|
+ *      ______________________________
+ * ...   write n bytes + ack  | stop |
+ *      ----------------------|------|
+ *
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+int32_t i2c_master_write_LSM6DSO(uint8_t i2c_num, uint8_t regaddr, uint8_t *data_wr, uint16_t size) {
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (LSM6DSO_ADDR_7BITS << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, regaddr, ACK_CHECK_EN);
+
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+int32_t i2c_master_write_LIS2MDL(uint8_t i2c_num, uint8_t regaddr, uint8_t *data_wr, uint16_t size) {
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (LIS2MDL_ADDR_7BITS << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, regaddr, ACK_CHECK_EN);
+
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Function : Initialize LSM6DSO sensor
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+esp_err_t lsm6dso_init(void) {
+	lsm6dso_dev_ctx.write_reg = i2c_master_write_LSM6DSO;
+	lsm6dso_dev_ctx.read_reg = i2c_master_read_LSM6DSO;
+	lsm6dso_dev_ctx.i2c_number = SENSOR_BUS;
+
+    /* Restore default configuration */
+    lsm6dso_reset_set(&lsm6dso_dev_ctx, PROPERTY_ENABLE);
+    do { lsm6dso_reset_get(&lsm6dso_dev_ctx, &LSM6DSO_rst); } while (LSM6DSO_rst);
+    /* Disable I3C interface */
+    lsm6dso_i3c_disable_set(&lsm6dso_dev_ctx, LSM6DSO_I3C_DISABLE);
+    /* Enable Block Data Update */
+    lsm6dso_block_data_update_set(&lsm6dso_dev_ctx, PROPERTY_ENABLE);
+    /* Set Output Data Rate */
+    lsm6dso_xl_data_rate_set(&lsm6dso_dev_ctx, LSM6DSO_XL_ODR_12Hz5);
+    lsm6dso_gy_data_rate_set(&lsm6dso_dev_ctx, LSM6DSO_GY_ODR_12Hz5);
+    /* Set full scale */
+    lsm6dso_xl_full_scale_set(&lsm6dso_dev_ctx, LSM6DSO_2g);
+    lsm6dso_gy_full_scale_set(&lsm6dso_dev_ctx, LSM6DSO_2000dps);
+    /* Configure filtering chain(No aux interface) */
+    /* Accelerometer - LPF1 + LPF2 path */
+    lsm6dso_xl_hp_path_on_out_set(&lsm6dso_dev_ctx, LSM6DSO_LP_ODR_DIV_100);
+    lsm6dso_xl_filter_lp2_set(&lsm6dso_dev_ctx, PROPERTY_ENABLE);
+
+    return ESP_OK;
+}
+
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Function : Initialize LIS2MDL sensor
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+esp_err_t lis2mdl_init(void) {
+	lis2mdl_dev_ctx.write_reg = i2c_master_write_LIS2MDL;
+	lis2mdl_dev_ctx.read_reg = i2c_master_read_LIS2MDL;
+	lis2mdl_dev_ctx.i2c_number = SENSOR_BUS;
+
+    /* Enable Block Data Update */
+    lis2mdl_block_data_update_set(&lis2mdl_dev_ctx, PROPERTY_ENABLE);
+	/* Set Output Data Rate */
+	lis2mdl_data_rate_set(&lis2mdl_dev_ctx, LIS2MDL_ODR_10Hz);
+	/* Set / Reset sensor mode */
+	lis2mdl_set_rst_mode_set(&lis2mdl_dev_ctx, LIS2MDL_SENS_OFF_CANC_EVERY_ODR);
+	/* Enable temperature compensation */
+	lis2mdl_offset_temp_comp_set(&lis2mdl_dev_ctx, PROPERTY_ENABLE);
+	/* Set device in continuous mode */
+	lis2mdl_operating_mode_set(&lis2mdl_dev_ctx, LIS2MDL_CONTINUOUS_MODE);
+
+	return ESP_OK;
+}
+
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Function : Get values from LSM6DSO sensor
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+void get_LSM6DSO() {
+    /* Read output only if new value is available */
+    lsm6dso_reg_t reg;
+    lsm6dso_status_reg_get(&lsm6dso_dev_ctx, &reg.status_reg);
+
+    if (reg.status_reg.xlda) {
+        memset(LSM6DSO_raw_acceleration, 0x00, 3 * sizeof(int16_t));
+        lsm6dso_acceleration_raw_get(&lsm6dso_dev_ctx, LSM6DSO_raw_acceleration);
+        LSM6DSO_acceleration_mg[0] = lsm6dso_from_fs2_to_mg(LSM6DSO_raw_acceleration[0]);
+        LSM6DSO_acceleration_mg[1] = lsm6dso_from_fs2_to_mg(LSM6DSO_raw_acceleration[1]);
+        LSM6DSO_acceleration_mg[2] = lsm6dso_from_fs2_to_mg(LSM6DSO_raw_acceleration[2]);
+        log_acceleration_X[-1] = LSM6DSO_acceleration_mg[0];
+        log_acceleration_Y[-1] = LSM6DSO_acceleration_mg[1];
+        log_acceleration_Z[-1] = LSM6DSO_acceleration_mg[2];
+        acc_index++;
+        sprintf((char *)tx_buffer, "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
+                LSM6DSO_acceleration_mg[0], LSM6DSO_acceleration_mg[1], LSM6DSO_acceleration_mg[2]);
+        printf((char const *)tx_buffer);
+    }
+
+    if (reg.status_reg.gda) {
+        memset(LSM6DSO_raw_angular_rate, 0x00, 3 * sizeof(int16_t));
+        lsm6dso_angular_rate_raw_get(&lsm6dso_dev_ctx, LSM6DSO_raw_angular_rate);
+        LSM6DSO_angular_rate_mdps[0] = lsm6dso_from_fs2000_to_mdps(LSM6DSO_raw_angular_rate[0]);
+        LSM6DSO_angular_rate_mdps[1] = lsm6dso_from_fs2000_to_mdps(LSM6DSO_raw_angular_rate[1]);
+        LSM6DSO_angular_rate_mdps[2] = lsm6dso_from_fs2000_to_mdps(LSM6DSO_raw_angular_rate[2]);
+        log_angular_rate_X[-1] = LSM6DSO_angular_rate_mdps[0];
+        log_angular_rate_Y[-1] = LSM6DSO_angular_rate_mdps[1];
+        log_angular_rate_Z[-1] = LSM6DSO_angular_rate_mdps[2];
+        gyro_index++;
+        sprintf((char *)tx_buffer, "Angular rate [mdps]:%4.2f\t%4.2f\t%4.2f\r\n",
+                LSM6DSO_angular_rate_mdps[0], LSM6DSO_angular_rate_mdps[1], LSM6DSO_angular_rate_mdps[2]);
+        printf((char const *)tx_buffer);
+    }
+
+    if (reg.status_reg.tda) {
+        memset(&LSM6DSO_raw_temperature, 0x00, sizeof(int16_t));
+        lsm6dso_temperature_raw_get(&lsm6dso_dev_ctx, &LSM6DSO_raw_temperature);
+        LSM6DSO_temperature_degC = lsm6dso_from_lsb_to_celsius(LSM6DSO_raw_temperature);
+        log_temperature[-1] = LSM6DSO_temperature_degC;
+        temp_index++;
+        sprintf((char *)tx_buffer, "Temperature [degC]:%6.2f\r\n", LSM6DSO_temperature_degC);
+        printf((char const *)tx_buffer);
+    }
+}
+
+/* --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--
+ * Function : Get values from LIS2MDL sensor
+ * --.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-- */
+void get_LIS2MDL() {
+    lis2mdl_reg_t reg;
+    lis2mdl_status_get(&lis2mdl_dev_ctx, &reg.status_reg);
+
+    if (reg.status_reg.zyxda) {
+        memset(LIS2MDL_raw_magnetic, 0x00, 3 * sizeof(int16_t));
+        lis2mdl_magnetic_raw_get(&lis2mdl_dev_ctx, LIS2MDL_raw_magnetic);
+        LIS2MDL_magnetic_mG[0] = lis2mdl_from_lsb_to_mgauss(LIS2MDL_raw_magnetic[0]);
+        LIS2MDL_magnetic_mG[1] = lis2mdl_from_lsb_to_mgauss(LIS2MDL_raw_magnetic[1]);
+        LIS2MDL_magnetic_mG[2] = lis2mdl_from_lsb_to_mgauss(LIS2MDL_raw_magnetic[2]);
+        log_magnetic_X[-1] = LIS2MDL_magnetic_mG[0];
+        log_magnetic_Y[-1] = LIS2MDL_magnetic_mG[1];
+        log_magnetic_Z[-1] = LIS2MDL_magnetic_mG[2];
+        mag_index++;
+        sprintf((char *)tx_buffer, "Magnetic field [mG]:%4.2f\t%4.2f\t%4.2f\r\n",
+                LIS2MDL_magnetic_mG[0], LIS2MDL_magnetic_mG[1], LIS2MDL_magnetic_mG[2]);
+        printf((char const *)tx_buffer);
+    }
+}
